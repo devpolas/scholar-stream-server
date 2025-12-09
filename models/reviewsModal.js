54d599a2b3500ca92fc80cd97f440aedbe2b0b71
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-12-09 18:42:19 +0600

    design review modal

---
+++ models/reviewsModal.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    scholarship: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Scholarship",
+      required: [true, "Review must belong to a scholarship"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+    comment: { type: String, required: [true, "Review can't be empty!"] },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Review must belong to a rating"],
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;

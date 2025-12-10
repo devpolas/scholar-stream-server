70b9cdc602ec953f54352bcdf32b556b8b241524
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-12-10 22:54:37 +0600

    handle application route as well

--- routes/applicationRouter.js
+++ routes/applicationRouter.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const authController = require("./../controllers/authController.js");
+const applicationController = require("./../controllers/applicationController.js");
 
 const router = express.Router({ mergeParams: true });
+
+router.use(authController.protect);
+
+router
+  .route("/")
+  .get(applicationController.getAllApplications)
+  .post(applicationController.createApplication);
+
+router
+  .route("/:id")
+  .get(applicationController.getApplication)
+  .delete(applicationController.deleteApplication)
+  .patch(
+    authController.restrictTo("admin", "moderator"),
+    applicationController.updateApplication
+  );
 
 module.exports = router;
